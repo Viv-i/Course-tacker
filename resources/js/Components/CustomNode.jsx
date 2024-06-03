69a4b201a3ac49231590d5143b318535c6305eb3
vi <feraloue@gmail.com>
2024-06-04 01:34:28 +0800

    fixed semantics

--- resources/js/Components/CustomNode.jsx
+++ resources/js/Components/CustomNode.jsx
@@ -3,17 +3,13 @@ import { Handle, Position } from 'reactflow';
 
 function CustomNode({ data }) {
   return (
-      <div className="px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-400">
-        <div className="flex">
-          <div className="ml-2">
-            <div className="text-lg font-bold">{data.code}</div>
-            <div className="text-gray-500">{data.description}</div>
-          </div>
-        </div>
+      <article className={`w-64 h-24 px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-400 flex-col ml-2 ${data.id}`}>
+            <h3 className="text-lg font-bold">{data.code}</h3>
+            <p className="text-gray-500  text-wrap">{data.description}</p>
 
         <Handle type="target" position={Position.Top} className="w-16 !bg-teal-500" />
         <Handle type="source" position={Position.Bottom} className="w-16 !bg-teal-500" />
-      </div>
+      </article>
   );
 }
 
